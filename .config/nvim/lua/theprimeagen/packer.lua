2558e6e1525d41c462e8bce8c00bfeb96a92dd67
Brian Hamilton <dabham271@gmail.com>
2025-04-08 03:11:52 +0000

    nvim: custom settings and add a hex color plugin.

--- .config/nvim/lua/theprimeagen/packer.lua
+++ .config/nvim/lua/theprimeagen/packer.lua
@@ -26,5 +26,6 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
+  use('brenoprata10/nvim-highlight-colors')
 
 end)
